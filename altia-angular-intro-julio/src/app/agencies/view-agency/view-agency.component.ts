21125195f6117426cf0bb5abe4e6ad64fc0716d2
alvaro.Manso <alvaromansorojas@gmail.com>
2022-07-14 11:00:29 +0200

    feat: services created

--- altia-angular-intro-julio/src/app/agencies/view-agency/view-agency.component.ts
+++ altia-angular-intro-julio/src/app/agencies/view-agency/view-agency.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/core/service/data.service';
+import { UtilService } from 'src/app/core/services/util.service';
 
 @Component({
   selector: 'app-view-agency',
@@ -7,9 +9,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./view-agency.component.css'],
 })
 export class ViewAgencyComponent implements OnInit {
-  public agencyId: string;
-  constructor(private route: ActivatedRoute) {
-    this.agencyId = this.route.snapshot.paramMap.get('id') || '';
+  // public agencyId: string;
+  public agency: any;
+  constructor(route: ActivatedRoute, util: UtilService, data: DataService) {
+    const agencyId = util.getIdFromRoute(route);
+
+    // this.agency = data.agencies.find(function byAgencyId(a) {
+    //   return a.id === agencyId;
+    // });
+
+    // const byAgencyId = (agency: any) => agency.id === agencyId;
+    // this.agency = data.agencies.find(byAgencyId);
+
+    this.agency = data.agencies.find((a) => a.id === agencyId);
+
+    // data.agencies.forEach((a) => console.log('agency:', a));
   }
 
   ngOnInit(): void {}
--- altia-angular-intro-julio/src/app/core/service/data.service.ts
+++ altia-angular-intro-julio/src/app/core/service/data.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor() { }
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+  constructor() {}
 }
